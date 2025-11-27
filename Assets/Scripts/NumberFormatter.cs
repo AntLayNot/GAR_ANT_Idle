52f415fbf00329da217a4e15f2fb12164ac441f9
AntLayNot <a.garcia@rns.ecolescreatives.com>
2025-11-27 17:59:23 +0100

    Ver.002

--- Assets/Scripts/NumberFormatter.cs
+++ Assets/Scripts/NumberFormatter.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public static class NumberFormatter
 {
     // K = mille, M = million, B = milliard, T = trillion, etc.
-    private static readonly string[] suffixes = { "", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No", "De", "Udc", "Ddc", "Tdc",
-    "Qadc", "Qidc", "Sxdc", "Spdc", "Ocdc", "Nmdc", "Vg", "Uvg", "Dvg", "Tvg", "Qav", "Qvg", "Svg", "Spv", "Ovg", "Nvg", "Tg"};
+    private static readonly string[] suffixes =
+    { "", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No",
+    "De", "Udc", "Ddc", "Tdc", "Qadc", "Qidc", "Sxdc", "Spdc", "Ocdc", "Nmdc",
+    "Vg", "Uvg", "Dvg", "Tvg", "Qav", "Qvg", "Svg", "Spv", "Ovg", "Nvg",
+    "Tg", "Qag", "Qig", "Sxg", "Spg", "Ocg", "Nog", "Ctg", "Uc", "Dc", "Tc",
+    "Qac", "Qic", "Sxc", "Spc", "Occ", "Noc", "Dc", "Ud", "Dd", "Td", "Qad",
+    "Qid", "Sxd", "Spd", "Ocd", "Nod", "Vd", "Uvd", "Dvd", "Tvd", "Qavd",
+    "Qivd", "Sxvd", "Spvd", "Ocvd", "Novd", "Tvd", "Qagd", "Qigd", "Sxgd",
+    "Spgd", "Ocgd", "Nogd", "Ctg", "Ucgd", "Dcgd", "Tcgd", "Qacgd", "Qicgd",
+    "Sxcgd", "Spcgd", "Occgd", "Nocgd", "Dcgd", "Udgd", "Ddgd", "Tdgd", "Qadgd" };
+
 
     public static string Format(double value)
     {
